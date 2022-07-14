434807c88ebee820b32b9809d615f4b60506c53f
ioannis_rom <ioannisromanelis@gmail.com>
2022-07-14 15:08:39 +0300

    ability to freeze/unfreeze backbone at  finetuning

--- finetune.py
+++ finetune.py
@@ -1,13 +1,14 @@
+from statistics import mean
+from sklearn.metrics import log_loss
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import pytorch_lightning as pl
 from pytorch_lightning.loggers import WandbLogger
+from pytorch_lightning import Callback
 from pl_bolts.optimizers.lr_scheduler import LinearWarmupCosineAnnealingLR
-
 from torchmetrics import Accuracy
 from dl_lib.datasets.ready_datasets import get_ModelNet40
-
 from modules import Group, TransformerWithEmbeddings
 
 def cal_loss(pred, gold, smoothing=True):
@@ -57,16 +58,23 @@ class Point_MAE_finetune_pl(pl.LightningModule):
         )
 
         self.cls_head = nn.Sequential(
-            nn.Linear(384, 512, bias=False),
-            nn.BatchNorm1d(512),
+            nn.Linear(2 * 384, 256, bias=False),
+            nn.BatchNorm1d(256),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.5),
+            nn.Linear(256, 256, bias=False),
+            nn.BatchNorm1d(256),
             nn.ReLU(),
-            nn.Linear(512, 40)
+            nn.Dropout(0.5),
+            nn.Linear(256, 40)
         )
 
     def forward(self, pts):
         neighborhood, center = self.group_devider(pts)
         x_vis = self.MAE_encoder(neighborhood, center)
-        feature_vector = torch.max(x_vis, dim=1).values
+        max_features = torch.max(x_vis, dim=1).values
+        mean_features = torch.mean(x_vis, dim=1)
+        feature_vector = torch.cat([max_features, mean_features], dim=-1)
         logits = self.cls_head(feature_vector)
         return logits
 
@@ -108,29 +116,53 @@ class Point_MAE_finetune_pl(pl.LightningModule):
         return [opt], [sched]
 
 
-    def load_submodules(self, path):
+    def load_submodules(self, path, freeze_backbone=True):
         # loading pretrained submodules
         checkpoint = torch.load(path)
         self.group_devider.load_state_dict(checkpoint['group_devider'])
         self.MAE_encoder.load_state_dict(checkpoint['MAE_encoder'])
 
+        if 'cls_head' in checkpoint.keys():
+            self.cls_head.load_state_dict(checkpoint['cls_head'])
+
         # freeze submodules
-        # for param in self.MAE_encoder.parameters():
-        #   param.requires_grad = False
+        if freeze_backbone:
+            self.freeze_backbone()
 
 
+    def freeze_backbone(self):
+        #print("freeze")
+        for param in self.MAE_encoder.parameters():
+            param.requires_grad = False
+
+    def unfreeze_backbone(self):
+        #print("unfreeze")
+        for param in self.MAE_encoder.parameters():
+            param.requires_grad = True
+
+
+
+class UnfreezeBackbone(Callback):
+    
+    def __init__(self, unfreeze_epoch):
+        self.unfreeze_epoch = unfreeze_epoch
+
+    def on_train_epoch_start(self, trainer: "pl.Trainer", pl_module: "pl.LightningModule"):
+        if trainer.current_epoch == self.unfreeze_epoch:
+            pl_module.unfreeze_backbone()
+            #print("Callback Working")
 
 
 if __name__ == "__main__":
 
     path = '/home/ioannis/Desktop/programming/phd/PCT_Pytorch/data'
-    train_loader, valid_loader = get_ModelNet40(path, 'original')
+    train_loader, valid_loader = get_ModelNet40(path, 'normalized')
 
     model = Point_MAE_finetune_pl()
-    model.load_submodules("/home/ioannis/Desktop/programming/phd/PointViT/Point-MAE/3arjutys/checkpoints/epoch=299-step=393300.ckpt")
+    model.load_submodules("/home/ioannis/Desktop/programming/phd/PointViT/custom_checkpoints/test.pt")
 
     project_name = "FINETUNING POINT_MAE" 
     logger = WandbLogger(project=project_name) 
 
-    trainer = pl.Trainer(accelerator='gpu', devices=1, max_epochs=300, logger=logger)
+    trainer = pl.Trainer(accelerator='gpu', devices=1, max_epochs=300, logger=logger, callbacks=[UnfreezeBackbone(100)])
     trainer.fit(model, train_loader, valid_loader)
